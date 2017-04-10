e1bc5943f769d5f1245fd2507fb38aab2da1a298
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-04-09 22:10:22 -0700

    Add controller specs

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe CampaignsController, type: :controller do
+  describe "GET #index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
 
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+    end
+  end
+
+  describe "GET /api_search" do
+    it "returns http success for an AJAX request" do
+      xhr :get, :api_search
+      expect(response).to be_ok
+    end
+  end
 end
