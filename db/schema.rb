fb2cc856ce44c060a62e5ee3ecc2d23a76ebbb60
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-04-11 19:28:45 -0700

    rip out sqlite for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170401172821) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "campaigns", force: :cascade do |t|
     t.string   "title"
     t.string   "small_image_url"
